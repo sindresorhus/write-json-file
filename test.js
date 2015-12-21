1d32d647e621543b2a0a8710a32b6e920e2f18fb
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-21 16:47:46 +0100

    fix tests

--- test.js
+++ test.js
@@ -6,17 +6,12 @@ import fn from './';
 
 test('async', async t => {
 	const tmp = path.join(tempfile(), 'foo');
-
 	await fn(tmp, {foo: true}, {indent: 2});
-
 	t.is(fs.readFileSync(tmp, 'utf8'), '{\n  "foo": true\n}\n');
 });
 
 test('sync', t => {
-	var tmp = path.join(tempfile(), 'foo');
-
+	const tmp = path.join(tempfile(), 'foo');
 	fn.sync(tmp, {foo: true}, {indent: 2});
-
 	t.is(fs.readFileSync(tmp, 'utf8'), '{\n  "foo": true\n}\n');
-	t.end();
 });
