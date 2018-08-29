3f8624e7701439a89a5b92a2521c33b2c0522429
Sam Verschueren <sam.verschueren@gmail.com>
2018-08-29 08:21:58 +0200

    Add empty line for example rendering

--- index.d.ts
+++ index.d.ts
@@ -39,6 +39,7 @@ export interface Options {
  * Creates directories for you as needed.
  *
  * @example
+ *
  * import * as writeJsonFile from 'write-json-file';
  *
  * writeJsonFile.sync('foo.json', {foo: true});
@@ -51,6 +52,7 @@ export function sync(filepath: string, data: JSONStringifyable, options?: Option
  * Creates directories for you as needed.
  *
  * @example
+ *
  * import writeJsonFile from 'write-json-file';
  *
  * (async () => {
