4aca7a2667e712dae78b2a29f780a2b2f586aadc
Kevin Martensson <kevinmartensson@gmail.com>
2019-01-20 00:33:36 +0100

    Reorder test input

--- test.js
+++ test.js
@@ -40,8 +40,8 @@ test('async - {sortKeys: true}', async t => {
 	const tmp = path.join(tempfile(), 'foo');
 	await m(tmp, {c: true, b: true, a: true}, {sortKeys: true});
 	t.is(fs.readFileSync(tmp, 'utf8'), '{\n\t"a": true,\n\t"b": true,\n\t"c": true\n}\n');
-	await m(tmp, ['a', 'b', 'c'], {sortKeys: true});
-	t.is(fs.readFileSync(tmp, 'utf8'), '[\n\t"a",\n\t"b",\n\t"c"\n]\n');
+	await m(tmp, ['c', 'b', 'a'], {sortKeys: true});
+	t.is(fs.readFileSync(tmp, 'utf8'), '[\n\t"c",\n\t"b",\n\t"a"\n]\n');
 });
 
 test('async - {sortKeys: false}', async t => {
