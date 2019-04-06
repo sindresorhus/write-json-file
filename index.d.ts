1081a1831db35192316ac59d9d159c2dec019951
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 15:52:50 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,66 +1,85 @@
-export type Replacer = (this: unknown, key: string, value: unknown) => unknown;
-export type SortKeys = (a: string, b: string) => number;
-export type JSONStringifyable = string | number | boolean | null | object;
+declare namespace writeJsonFile {
+	type Replacer = (this: unknown, key: string, value: any) => unknown;
+	type SortKeys = (a: string, b: string) => number;
+	type JSONStringifyable = string | number | boolean | null | object;
 
-export interface Options {
-	/**
-	 * Indentation as a string or number of spaces. Pass in null for no formatting.
-	 *
-	 * @default '\t'
-	 */
-	readonly indent?: string | number | null;
+	interface Options {
+		/**
+		Indentation as a string or number of spaces. Pass in null for no formatting.
 
-	/**
-	 * Detect indentation automatically if the file exists.
-	 *
-	 * @default false
-	 */
-	readonly detectIndent?: boolean;
+		@default '\t'
+		*/
+		readonly indent?: string | number | null;
 
-	/**
-	 * Sort the keys recursively. Optionally pass in a compare function.
-	 *
-	 * @default false
-	 */
-	readonly sortKeys?: boolean | SortKeys;
+		/**
+		Detect indentation automatically if the file exists.
 
-	/**
-	 * Passed into `JSON.stringify`.
-	 */
-	readonly replacer?: Replacer | Array<number | string>;
+		@default false
+		*/
+		readonly detectIndent?: boolean;
 
-	/**
-	 * Mode used when writing the file.
-	 *
-	 * @default 0o666
-	 */
-	readonly mode?: number;
+		/**
+		Sort the keys recursively. Optionally pass in a compare function.
+
+		@default false
+		*/
+		readonly sortKeys?: boolean | SortKeys;
+
+		/**
+		Passed into `JSON.stringify`.
+		*/
+		readonly replacer?: Replacer | Array<number | string>;
+
+		/**
+		Mode used when writing the file.
+
+		@default 0o666
+		*/
+		readonly mode?: number;
+	}
 }
 
-/**
- * Stringify and write JSON to a file atomically.
- *
- * Creates directories for you as needed.
- *
- * @example
- *
- * import * as writeJsonFile from 'write-json-file';
- *
- * writeJsonFile.sync('foo.json', {foo: true});
- */
-export function sync(filepath: string, data: JSONStringifyable, options?: Options): void;
+declare const writeJsonFile: {
+	/**
+	Stringify and write JSON to a file atomically.
 
-/**
- * Stringify and write JSON to a file atomically.
- *
- * Creates directories for you as needed.
- *
- * @example
- *
- * import writeJsonFile from 'write-json-file';
- *
- * (async () => {
- * 	await writeJsonFile('foo.json', {foo: true});
- * })();
- */
-export default function writeJsonFile(filepath: string, data: JSONStringifyable, options?: Options): Promise<void>;
+	Creates directories for you as needed.
+
+	@example
+	```
+	import writeJsonFile = require('write-json-file');
+
+	(async () => {
+		await writeJsonFile('foo.json', {foo: true});
+	})();
+	```
+	*/
+	(
+		filepath: string,
+		data: writeJsonFile.JSONStringifyable,
+		options?: writeJsonFile.Options
+	): Promise<void>;
+
+	/**
+	Stringify and write JSON to a file atomically.
+
+	Creates directories for you as needed.
+
+	@example
+	```
+	import writeJsonFile = require('write-json-file');
+
+	writeJsonFile.sync('foo.json', {foo: true});
+	```
+	*/
+	sync(
+		filepath: string,
+		data: writeJsonFile.JSONStringifyable,
+		options?: writeJsonFile.Options
+	): void;
+
+	// TODO: Remove this for the next major release
+	default: typeof writeJsonFile;
+};
+
+export = writeJsonFile;
